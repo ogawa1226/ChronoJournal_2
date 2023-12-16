e7626d351889541a7e261cbf4c8212d238d95fd5
Ogawa <145567537+ogawa1226@users.noreply.github.com>
2023-12-16 09:51:16 +0000

    カレンダー　レイアウト

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,4 +3,13 @@ class CalendarsController < ApplicationController
     @date = params[:date]
     @schedules = Schedule.all
   end
+  
+  before_action :set_beginning_of_week
+  
+  private
+  
+  def set_beginning_of_week
+    Date.beginning_of_week = :sunday
+  end
+  
 end
