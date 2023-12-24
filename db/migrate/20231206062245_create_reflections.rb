c38701aacc6228b8e92e0b31ad85ccbf752241ba
Ogawa <145567537+ogawa1226@users.noreply.github.com>
2023-12-24 10:26:38 +0000

    微修正

--- db/migrate/20231206062245_create_reflections.rb
+++ db/migrate/20231206062245_create_reflections.rb
@@ -3,7 +3,7 @@ class CreateReflections < ActiveRecord::Migration[6.1]
     create_table :reflections do |t|
       
       t.integer :schedule_id, null: false
-      
+      t.integer :user_id, null: false
       t.string :title, null: false
       t.text :content, null: false
       
